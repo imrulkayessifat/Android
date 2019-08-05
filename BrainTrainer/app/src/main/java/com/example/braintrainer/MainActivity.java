a283bfe2e83e326db475dbfd6b45b78c85404c5f
ImrulKay3s <kayesimrul224@gmail.com>
2019-08-05 06:57:51 -0800

    Basics Android

---
+++ BrainTrainer/app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.braintrainer;
+
+import android.app.usage.NetworkStats;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button playAgainButton;
+    TextView sumTextView;
+    TextView resultTextView;
+    TextView pointsTextView;
+    TextView timerTextView;
+    RelativeLayout gameRelativeLayout;
+    ArrayList<Integer>answers = new ArrayList<Integer>();
+    int locationOfCorrectAnswer;
+    int score = 0;
+    int numberOfQuestion = 0;
+
+    public void playAgain(View view){
+        score = 0;
+        numberOfQuestion = 0;
+        timerTextView.setText("30s");
+        pointsTextView.setText("0/0");
+        resultTextView.setText("");
+        playAgainButton.setVisibility(View.INVISIBLE);
+        generateQuestion();
+        new CountDownTimer(30100, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText(String.valueOf(millisUntilFinished /1000) + "s");
+            }
+
+            @Override
+            public void onFinish() {
+                playAgainButton.setVisibility(View.VISIBLE);
+                timerTextView.setText("0s");
+                resultTextView.setText("Your Score : " + Integer.toString(score) + "/" + Integer.toString(numberOfQuestion));
+            }
+        }.start();
+    }
+
+    public void generateQuestion(){
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + "+" + Integer.toString(b));
+
+        locationOfCorrectAnswer = rand.nextInt(4);
+        answers.clear();
+        int incorrectAnswer;
+
+        for(int i =0;i<4;i++){
+            if(i==locationOfCorrectAnswer){
+                answers.add(a + b);
+            }
+            else{
+                incorrectAnswer = rand.nextInt(41);
+                while(incorrectAnswer == a + b){
+                    incorrectAnswer = rand.nextInt(41);
+                }
+                answers.add(incorrectAnswer);
+            }
+        }
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
+    }
+
+    public void chooseAnswer(View view){
+
+        if(view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))){
+            score++;
+            resultTextView.setText("Correct!");
+        }else{
+            resultTextView.setText("Wrong!");
+        }
+        numberOfQuestion++;
+        pointsTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestion));
+        generateQuestion();
+    }
+
+    public void start(View view){
+        startButton.setVisibility(View.INVISIBLE);
+        gameRelativeLayout.setVisibility(RelativeLayout.VISIBLE);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton = (Button)findViewById(R.id.startButton);
+        button0 = (Button)findViewById(R.id.button0);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        playAgainButton = (Button)findViewById(R.id.playAgainButton);
+        sumTextView = (TextView)findViewById(R.id.sumTextView);
+        resultTextView = (TextView)findViewById(R.id.resultTextView);
+        pointsTextView = (TextView)findViewById(R.id.pointsTextView);
+        timerTextView = (TextView)findViewById(R.id.timerTextView);
+        gameRelativeLayout = (RelativeLayout)findViewById(R.id.gameRelativeLayout);
+        playAgain(findViewById(R.id.timerTextView));
+        generateQuestion();
+    }
+}
---
+++ DownloadingImages/app/src/main/java/com/example/downloadingimages/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.downloadingimages;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView downloadImg;
+
+    public void downloadImage(View view){
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myImage;
+        try {
+            myImage = task.execute("https://images-na.ssl-images-amazon.com/images/I/61Lfb%2Bsv5IL._SY679_.jpg").get();
+            downloadImg.setImageBitmap(myImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.i("Interaction","Button Tapped");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        downloadImg = (ImageView)findViewById(R.id.imageView);
+    }
+    public class ImageDownloader extends AsyncTask<String,Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+    }
+}
